03488469d9990b0e2bc593a39a0525e56047d741
04756 <31569881+04756@users.noreply.github.com>
2017-09-02 23:53:49 +0800

    Update everyday.js

--- static/assets/js/everyday.js
+++ static/assets/js/everyday.js
@@ -39,15 +39,15 @@ $(document).ready(function(){
   $(".photo").click(function(){
   if(a ==1)
   {
-    $(".introduce").hide();
-    $(".photo").css({"width":"250px","height":"250px"})
-    $(".photo img").css({"width":"250px","height":"250px"})
+    $(this).find(".introduce").hide();
+    $(this).css({"width":"250px","height":"250px"});
+    $(this).find("img").css({"width":"250px","height":"250px"});
     a=2;
   }
   else{
-    $(".introduce").show();
-    $(".photo").css({"width":"70px","height":"70px"})
-    $(".photo img").css({"width":"70px","height":"70px"})
+    $(this).find(".introduce").show();
+    $(this).css({"width":"70px","height":"70px"});
+    $(this).find("img").css({"width":"70px","height":"70px"});
     a=1;
   }
   });
