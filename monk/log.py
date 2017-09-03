987478c91ee4bdcf8e84ecfab085e4bb6e6d1b0b
suchang <1103232282@qq.com>
2017-09-03 22:56:18 +0800

    add uvloop

--- monk/log.py
+++ monk/log.py
@@ -1,3 +1,8 @@
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='monk.log',
+                    filemode='w'
+                    )
 log = logging.getLogger(__name__)
